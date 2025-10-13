54226e37011591c9b02067e42d346416ff6a2ce9
Marilia84 <mariliateixeirainacio94@gmail.com>
2025-10-13 07:55:54 -0300

    adicionando layout no home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,11 @@ export default function Home() {
     window.location.href = "/"; // redireciona para login
   };
   return( 
-  <div className="bg-gradient-to-b from-white to-[#E1EDE7]  flex flex-col justify-center  h-screen gap-4">
+  <div className="relative bg-[#E6E6E6] min-h-screen flex items-center gap-4 overflow-hidden">
+    <div className="absolute right-0 bottom-10 w-[650px] h-[600px] bg-[#53A67F] rounded-tl-[400px] rounded-bl-[300px] rotate-[20deg] translate-x-1/4 -translate-y-1/4 z-0" />
+    <div className="absolute right-0 top-[750px] w-[950px] h-[450px] bg-[#53A67F]  rounded-tl-[400px] rounded-br-[400px] rounded-tr-[400px] rotate-[-10deg] translate-x-1/4 -translate-y-1/4 z-0" />
+
+      
       <Navbar />
      
     </div>
