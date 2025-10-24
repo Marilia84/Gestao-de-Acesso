48dd2833b10bf182a54621d43662725afce1bfca
Marilia84 <mariliateixeirainacio84@gmail.com>
2025-10-23 22:10:23 -0300

    Mapa do google funcionando

--- src/components/GoogleMapaRota.jsx
+++ src/components/GoogleMapaRota.jsx
@@ -6,7 +6,7 @@ import {
   Polyline,
   useJsApiLoader,
 } from "@react-google-maps/api";
-
+const GOOGLE_LIBRARIES = ["places"];
 const containerStyle = { width: "100%", height: "100%" };
 
 const polylineOptions = {
